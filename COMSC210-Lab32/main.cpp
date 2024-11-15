7084fa4e6c06f688233cb49a669566178d7c4592
Austin <67823650+muster-Wugui@users.noreply.github.com>
2024-11-15 10:05:06 -0800

    Third Commit

--- COMSC210-Lab32/main.cpp
+++ COMSC210-Lab32/main.cpp
@@ -13,9 +13,7 @@ using namespace std;
 int main() {
     srand(static_cast<unsigned int>(time(0)));
     
-   array<deque<Car>, 4> lanes;
-    
-    int time = 0;
+    array<deque<Car>, 4> lanes;
 
     for (auto& lane : lanes) {
         int numCars = rand() % 3 + 1;
@@ -33,9 +31,9 @@ int main() {
             cout << "\tEmpty\n";
         }
         else{
-           for (const auto& car : lanes[i]){
+           for (auto& car : lanes[i]){
                cout << "\t";
-                car.print();
+            car.print();
            }
         }
 
@@ -46,12 +44,41 @@ int main() {
         for(int i = 0; i < 4; i++){
             int rad = rand() % 100;
             cout<<"Lane "<<i+1<<":"<<endl;
-            
+
+            if(!lanes[i].empty()){
+                if(rad < 46){
+                    cout << "Paid: ";
+                    lanes[i].front().print();
+                    lanes[i].pop_front();
+                }
+                else if (rad < 85){
+                    Car newCar;
+                    cout << "Joined: ";
+                    newCar.print();
+                    lanes[i].push_back(newCar);
+                }else{
+                    int newlane;
+                    do {
+                        newlane = rand() % 4; // Pick a new random lane
+                    } while (newlane == i);
+
+                    Car switchcar = lanes[i].back();
+                    lanes[i].pop_back();
+                    lanes[newlane].push_back(switchcar);
+
+                    cout << "Switched: ";
+                    switchcar.print();
+                    cout << " to Lane " << newlane + 1;
+                }
+            }
+            else{
+                if(rand() % 2 == 0){
+                    
+                }
+            }
         }
     }
-
-    }
-
+    
     return 0;
-}
+    }
 
