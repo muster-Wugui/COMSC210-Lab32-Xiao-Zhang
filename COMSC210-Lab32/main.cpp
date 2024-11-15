288ed4816b0cfd9774df35fa93852da80b91d72a
muster-Wugui <tintinpost28@gmail.com>
2024-11-14 17:11:15 -0800

    Initial Commit

---
+++ COMSC210-Lab32/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COMSC210-Lab32
+//
+//  Created by Xiao Zhang on 11/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
